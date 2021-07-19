3b80ee7b9cb7f9e7b361ef8120522fd42f529b0f
Alexandr Halypa <gwair1989@gmail.cm>
2021-07-20 01:38:51 +0300

    Change setUI

--- Left & Right i Code/ViewController.swift
+++ Left & Right i Code/ViewController.swift
@@ -17,7 +17,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         setUI()
     }
 
@@ -49,7 +48,6 @@ class ViewController: UIViewController {
         
         view.backgroundColor = .systemOrange
 
-        
         view.addSubview(leftButton)
         leftButton.frame = CGRect(x: 10, y: view.frame.height / 1.3, width: view.frame.width / 2 - 10, height: 130)
         leftButton.backgroundColor = .systemIndigo
@@ -72,72 +70,38 @@ class ViewController: UIViewController {
         let width = view.frame.width / 3 - 20
         let heigth = width
         
-        view.addSubview(image[0])
-        image[0].frame = CGRect(x: 10, y: view.frame.height / 9, width: width, height: heigth)
-        image[0].image = UIImage(named: "Exe")
-        print("Wight 1image\(image[0].frame.width)")
-        
-        view.addSubview(image[1])
-        image[1].frame = CGRect(x: view.frame.width / 3 + 10, y: view.frame.height / 9, width: width, height: heigth)
-        image[1].image = UIImage(named: "Like")
-        print("Wight 1image\(image[1].frame.width)")
-        
-        view.addSubview(image[2])
-        image[2].frame = CGRect(x: view.frame.width - width - 10, y: view.frame.height / 9, width: width, height: heigth)
-        image[2].image = UIImage(named: "Like")
-        print("Wight 1image\(image[2].frame.width)")
-        
-        
-        
-        view.addSubview(image[3])
-        image[3].frame = CGRect(x: 10, y: view.frame.height / 4, width: width, height: heigth)
-        image[3].image = UIImage(named: "Like")
-        print("Wight 1image\(image[0].frame.width)")
-        
-        
-        view.addSubview(image[4])
-        image[4].frame = CGRect(x: view.frame.width / 3 + 10, y: view.frame.height / 4, width: width, height: heigth)
-        image[4].image = UIImage(named: "Like")
-        print("Wight 1image\(image[1].frame.width)")
-        
-        view.addSubview(image[5])
-        image[5].frame = CGRect(x: view.frame.width - width - 10, y: view.frame.height / 4, width: width, height: heigth)
-        image[5].image = UIImage(named: "Like")
-        print("Wight 1image\(image[2].frame.width)")
-        
-        
-        view.addSubview(image[6])
-        image[6].frame = CGRect(x: 10, y: view.frame.height / 2.55, width: width, height: heigth)
-        image[6].image = UIImage(named: "Like")
-        print("Wight 1image\(image[0].frame.width)")
-        
-        
-        view.addSubview(image[7])
-        image[7].frame = CGRect(x: view.frame.width / 3 + 10, y: view.frame.height / 2.55, width: width, height: heigth)
-        image[7].image = UIImage(named: "Like")
-        print("Wight 1image\(image[1].frame.width)")
-        
-        view.addSubview(image[8])
-        image[8].frame = CGRect(x: view.frame.width - width - 10, y: view.frame.height / 2.55, width: width, height: heigth)
-        image[8].image = UIImage(named: "Like")
-        print("Wight 1image\(image[2].frame.width)")
-        
-        
-        view.addSubview(image[9])
-        image[9].frame = CGRect(x: 10, y: view.frame.height / 1.88, width: width, height: heigth)
-        image[9].image = UIImage(named: "Like")
-        print("Wight 1image\(image[0].frame.width)")
-        
-        view.addSubview(image[10])
-        image[10].frame = CGRect(x: view.frame.width / 3 + 10, y: view.frame.height / 1.88, width: width, height: heigth)
-        image[10].image = UIImage(named: "Like")
-        print("Wight 1image\(image[1].frame.width)")
-        
-        view.addSubview(image[11])
-        image[11].frame = CGRect(x: view.frame.width - width - 10, y: view.frame.height / 1.88, width: width, height: heigth)
-        image[11].image = UIImage(named: "Like")
-        print("Wight 1image\(image[2].frame.width)")
 
+        image[0].frame = CGRect(x: 10, y: view.frame.height / 9, width: width, height: heigth)
+
+        image[1].frame = CGRect(x: view.frame.width / 3 + 10, y: view.frame.height / 9, width: width, height: heigth)
+
+        image[2].frame = CGRect(x: view.frame.width - width - 10, y: view.frame.height / 9, width: width, height: heigth)
+
+        image[3].frame = CGRect(x: 10, y: view.frame.height / 4, width: width, height: heigth)
+
+        image[4].frame = CGRect(x: view.frame.width / 3 + 10, y: view.frame.height / 4, width: width, height: heigth)
+
+
+        image[5].frame = CGRect(x: view.frame.width - width - 10, y: view.frame.height / 4, width: width, height: heigth)
+        
+        image[6].frame = CGRect(x: 10, y: view.frame.height / 2.55, width: width, height: heigth)
+
+        image[7].frame = CGRect(x: view.frame.width / 3 + 10, y: view.frame.height / 2.55, width: width, height: heigth)
+
+        image[8].frame = CGRect(x: view.frame.width - width - 10, y: view.frame.height / 2.55, width: width, height: heigth)
+
+        image[9].frame = CGRect(x: 10, y: view.frame.height / 1.88, width: width, height: heigth)
+
+        image[10].frame = CGRect(x: view.frame.width / 3 + 10, y: view.frame.height / 1.88, width: width, height: heigth)
+
+        image[11].frame = CGRect(x: view.frame.width - width - 10, y: view.frame.height / 1.88, width: width, height: heigth)
+
+        
+        for image in image{
+            image.image = UIImage(named: "Like")
+            self.image[0].image = UIImage(named: "Exe")
+            view.addSubview(image)
+        }
         
     }
     
